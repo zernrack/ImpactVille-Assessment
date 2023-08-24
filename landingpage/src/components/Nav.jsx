871f77a62f699cf2775944a912ee03faf504860f
Renz Rackhold <rackhold@gmail.com>
2023-08-25 00:53:14 +0800

    Update Nav link redirects

--- landingpage/src/components/Nav.jsx
+++ landingpage/src/components/Nav.jsx
@@ -1,31 +1,31 @@
 function Nav() {
   return (
     <>
-      <nav className="flex flex-col w-full px-6 py-4 font-sans text-center bg-gray-300 shadow sm:flex-row sm:text-left sm:justify-between sm:items-baseline">
+      <nav className="flex flex-col w-full px-6 py-4 font-sans text-center shadow dark:bg-gray-800 sm:flex-row sm:text-left sm:justify-between sm:items-baseline">
         <div className="mb-2 sm:mb-0">
           <a
             href="#"
-            className="text-2xl no-underline text-grey-darkest hover:text-blue-dark "
+            className="text-2xl font-extrabold text-white no-underline hover:text-blue-dark "
           >
             TechnoWorks
           </a>
         </div>
         <div className="flex gap-4">
           <a
-            href="#"
-            className="ml-2 text-lg no-underline text-grey-darkest hover:text-blue-dark hover:underline"
+            href="#home"
+            className="ml-2 text-lg font-semibold text-white no-underline hover:text-blue-dark hover:underline"
           >
             Home
           </a>
           <a
             href="#"
-            className="ml-2 text-lg no-underline text-grey-darkest hover:text-blue-dark hover:underline"
+            className="ml-2 text-lg font-semibold text-white no-underline hover:text-blue-dark hover:underline"
           >
             About
           </a>
           <a
             href="#"
-            className="ml-2 text-lg no-underline text-grey-darkest hover:text-blue-dark hover:underline"
+            className="ml-2 text-lg font-semibold text-white no-underline hover:text-blue-dark hover:underline"
           >
             Products
           </a>
